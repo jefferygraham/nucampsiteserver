af4e20c3c57952092d80c4702356af64231f3340
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-12-06 20:50:08 -0500

    .gitignore is now working

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/nucampsite',
-  facebook: {
-    clientId: '208614594154059',
-    clientSecret: 'b774ee0102bc2a358b67389b5c57e743',
-  },
-};
