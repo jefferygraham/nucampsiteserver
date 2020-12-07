89038f9f3ce97398ecd0d602ddc7073e0b2edca5
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-12-06 20:40:00 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/nucampsite',
+  facebook: {
+    clientId: '208614594154059',
+    clientSecret: 'b774ee0102bc2a358b67389b5c57e743',
+  },
 };
